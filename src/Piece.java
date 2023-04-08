c5efa0d92f84fa6d87cba6ea81434981d97abf37
maria menezes <diogo0254jf@gmail.com>
2023-04-08 19:15:39 -0300

    Add Board and Piece

---
+++ src/Piece.java
@@ -0,0 +1,15 @@
+public class Piece {
+    protected Position position;
+    protected Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    
+}
